c28dedf37ec715a4060f62b6e50211eacfea39ca
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-02 19:32:58 +0700

    Upload main.py via PY to EXE tool

---
+++ python-files/main.py
@@ -0,0 +1,38 @@
+
+#importing stuff
+import time
+import turtle
+print("launching")
+turtle.forward(0)
+#defining
+t = turtle
+
+
+def w():
+    t.forward(10)
+def a():
+    t.left(90)
+def s():
+    t.backward(10)
+def d():
+    t.right(90)
+
+
+t.bgpic("i.png")
+
+t.onkey(w, "w")
+t.onkey(a, "a")
+t.onkey(s, "s")
+t.onkey(d, "d")
+
+
+#actions
+t.listen()
+
+input()
+
+
+
+
+
+
