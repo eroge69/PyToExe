832746d57077eb0794f4ff6a00e9c30a1576e3b5
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-15 17:15:58 +0700

    Upload case.py via PY to EXE tool

---
+++ python-files/case.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+def n():
+    number = random.randint(1, 100)
+    print(f"УДАЧИ!")
+    time.sleep(0.5)
+    print(f"ПРОКРУТКА.")
+    time.sleep(0.5)
+    print(f"ПРОКРУТКА..")
+    time.sleep(0.5)
+    print(f"ПРОКРУТКА...")
+    time.sleep(0.5)
+    print(f"ПРОКРУТКА....")
+    time.sleep(0.5)
+    print(f"ПРОКРУТКА.....")
+    time.sleep(2)
+    print("======================")
+    print(f"ПРОЦЕНТ {number}%")
+    print("======================")
+    
+    if number > 50:
+        print("L")
+        return input("ДОДЕП? (да/нет): ").lower() == 'да'
+
+    else:
+        print("W")
+        return input("ВЫВОД? (да/нет): ").lower() == 'нет'
+
+def main():
+    print("============================ CASE BATTLE BETA 0.1 =============================")
+    print("<= 50% --- W | > 50% --- L")
+    input("НАЖМИ ENTER ДЛЯ ПРОКРУТА")
+    
+    while True:
+        if not n():
+            print("bb")
+            break
+
+if __name__ == "__main__":
+    main()
