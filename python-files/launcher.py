a743a4c7d878d9155d1d73be4c2b7a6e404e7ef7
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-21 22:34:33 +0700

    Upload launcher.py via PY to EXE tool

---
+++ python-files/launcher.py
@@ -0,0 +1,6 @@
+import os
+
+# This launcher is designed to run your Streamlit app when built as an EXE
+# You can upload this file to https://v0-pytoexe.vercel.app/ for EXE conversion
+
+os.system("streamlit run \"tube calculator app-3.2.py\"")
