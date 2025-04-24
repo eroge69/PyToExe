fc39d2cfb27621b74c7c60269d0bdb362dbf0f75
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-24 15:48:23 +0700

    Upload sample_script.py via PY to EXE tool

---
+++ python-files/sample_script.py
@@ -0,0 +1,10 @@
+# sample_script.py
+import time
+
+def main():
+    print("Hello, this is a sample Python script!")
+    time.sleep(2)  # Waits for 2 seconds
+    print("This script will be converted to an EXE file.")
+
+if _name_ == "_main_":
+    main()
