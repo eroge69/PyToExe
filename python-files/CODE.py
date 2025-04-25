a585f256b4464ec168f109b7bed4f2e73e98ad7e
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-25 21:15:44 +0700

    Upload CODE.py via PY to EXE tool

---
+++ python-files/CODE.py
@@ -0,0 +1,8 @@
+sarmaye = float(input('sarmaye?'))
+riskrozane = float(input('riskrozane?'))
+ahrom = float(input('ahrom?'))
+riskmoamele = float(input('riskrmoamele?'))
+l = sarmaye*riskrozane/100
+r = ahrom*riskmoamele/100
+a = l/r
+print(a)
