41d244b54dbf88145b9c86ea66a1012e1f291f0b
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-26 19:24:54 +0700

    Upload py.py via PY to EXE tool

---
+++ python-files/py.py
@@ -0,0 +1,9 @@
+Player1 = int(input("Введите ваше первое число!: "))
+Player2 = int(input("Введите ваше второе число!: "))
+
+print("Result: ", Player1 + Player2)
+print("Result: ", Player1 - Player2)
+print("Result: ", Player1 * Player2)
+print("Result: ", Player1 / Player2)
+print("Result: ", Player1 // Player2)
+print("Result: ", Player1 ** Player2)
