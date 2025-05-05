1767df0d792897dc7e9e09d1f8dad2beadb58cfc
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-05 17:52:06 +0700

    Upload reverse_text.py via PY to EXE tool

---
+++ python-files/reverse_text.py
@@ -0,0 +1,9 @@
+```python
+def reverse_text(text):
+    return text[::-1]
+if __name__ == "__main__":
+    text = input("Введите текст для переворачивания: ")
+    reversed_text = reverse_text(text)
+    print("Перевёрнутый текст: ", reversed_text)
+```
+
