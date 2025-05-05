1da1562e82c514105279fe26e4fee3b56b1dbd17
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-05 21:54:50 +0700

    Upload dont run.py via PY to EXE tool

---
+++ python-files/dont run.py
@@ -0,0 +1,11 @@
+public class MyClass {
+  public static void main(String[] args) {
+    int hedgehogs = 5;
+
+    while (hedgehogs < 6) {
+
+      System.out.println("Not enough hedgehogs!");
+
+    }
+  }
+}
