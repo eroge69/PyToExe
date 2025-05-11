3c0b49906dccc5e215871b3e6f0bd666c5492d41
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-12 00:47:46 +0700

    Upload 01-pierwszy-plik.py via PY to EXE tool

---
+++ python-files/01-pierwszy-plik.py
@@ -0,0 +1,9 @@
+name = input( "jak masz na imię? ")
+print("czesc " +  name + "!")
+
+last_name = input( "jak masz na nazwisko? ")
+print("ładne nazwisko " + last_name )
+
+age = input("podaj swój wiek ")
+print("stary jesteś" )
+
