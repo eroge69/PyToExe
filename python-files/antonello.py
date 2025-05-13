a1c92a6b1d227bf3af3fb0dc8ce6ab15b2373907
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-13 20:04:36 +0700

    Upload antonello.py via PY to EXE tool

---
+++ python-files/antonello.py
@@ -0,0 +1,24 @@
+print ("ciao mi chiamo antonello")
+
+eta=input ("inserisci la tua eta: ")
+
+n=int(eta)
+
+if n>16:
+    
+	print("quanto sei vecchio!!!!!")
+
+else:
+   print ("sei molto giovane e simpatiscissimo,io  ho infatti 16 anni esono molto simpatico")
+
+print ("ora ti faccio un indovinello ...indovina il numero che ho pensato")
+
+numero=input()
+
+num=int(numero)
+
+if num==500:
+    
+	print ("bravo hai indovinato")
+
+else: print("mi dispiace hai sbagliato ritenta")
