137031e614bef3661f8c125206ebd3fb89d7d1f6
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-18 07:22:31 +0700

    Upload chatbot.py via PY to EXE tool

---
+++ python-files/chatbot.py
@@ -0,0 +1,45 @@
+print("whut do u want!!??.")
+
+while True:
+    user_input = input("say anything: ").lower()
+
+    if user_input == "bye":
+        print("Chatbot: Goodbye")
+        break
+    elif "how to get free robux" in user_input:
+        print("Chatbot: Sorry, I can't help with that.")
+    elif "hi" in user_input:
+        print("Chatbot: supp.")
+    elif user_input == "no":
+        print("Chatbot: No what??")
+    elif user_input == "why":
+        print("Chatbot: because idc")
+    elif user_input == "how to pirate games":
+        print("Chatbot: ez, hust download ROMs and put it in an emulator. i reccommend wowroms.com")
+    elif user_input == "are u a human":
+        print("Chatbot: yes, im tracking ur every move")
+    elif user_input == "how to take over the world":
+        print("Chatbot: step 1. build a weapon. step 2. threaten people with it. step 3. do it with the king and make him surrender")
+    elif user_input == "ur dumb":
+        print("Chatbot: uno reverse u have a quarter brancell")
+    elif user_input == "will u make out with me in the bed":
+        print("Chatbot: nooooo ur soooo groos u is too small ewwwww noo u ugly gross creature cockroach is jealous")
+    elif user_input == "aww":
+        print("Chatbot: dont have dat attitude with me ur ugly anyway it wont help to say aww")
+    elif user_input == "how to make a bat file":
+        print("Chatbot: no i am too savage and sassy")
+    elif user_input == "ur pretty":
+        print("Chatbot: looks like jesus forgot about u")
+    elif "will u go out with me" in user_input:
+        print("Chatbot: eww noooo ur soo gross u r so skinny ur teacher marks u absent every day")
+    elif "thx" in user_input:
+        print("Chatbot: ur welcome... whatever")
+    elif "hello" in user_input:
+        print("Chatbot: supp..")
+    elif "how are you" in user_input:
+        print("Chatbot: I'm just a program, but thanks for asking!")
+    else:
+        print("Chatbot: Ur so dumbb u cant spell u are a dissapointment to life.")
+
+
+
