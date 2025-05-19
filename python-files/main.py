05f4db213ac45b7bac92df5c4331df0e8fe8120a
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-19 23:37:42 +0700

    Upload main.py via PY to EXE tool

---
+++ python-files/main.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+
+from classes.logger import Logger
+from gui.main_window import MainWindow
+from classes.trigger_bot import CS2TriggerBot
+
+def main():
+    # Set up logging for the application.
+    Logger.setup_logging()
+    logger = Logger.get_logger()
+
+    # Initialize the QApplication. If an instance already exists, use it; otherwise, create a new one.
+    app = QApplication.instance() or QApplication(sys.argv)
+
+    # Log the loaded version.
+    logger.info("Loaded version: %s", CS2TriggerBot.VERSION)
+
+    # Create and display the main application window.
+    window = MainWindow()
+    window.show()
+
+    # Start the application event loop and exit with the returned status.
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
