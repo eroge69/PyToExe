d478b3b81f060ce2024f0289dd685b3ffa55ec59
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-21 21:08:49 +0700

    Upload profile_formula.py via PY to EXE tool

---
+++ python-files/profile_formula.py
@@ -0,0 +1,27 @@
+import math
+
+h=float(input('ertefa ra vared konid: '))
+tool=float(input('andaze zel ra vared konid: '))
+L1=0
+if h<=100:
+   L1=38
+if 100<h and h<=160:
+   L1=35
+if h>160:
+   L1=32
+
+
+L2=L1+4
+
+x=math.floor((tool-17)/L1)
+
+if (tool-17)/x > L2:
+   n=x+2
+else: 
+   n=x+1
+
+
+
+print('tedad profile: ',n)
+tool=float(input(''))
+  
