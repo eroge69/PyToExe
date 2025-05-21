8d9150297d043289e3fdd41db023057f70c987b9
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-22 06:13:08 +0700

    Upload main (2).py via PY to EXE tool

---
+++ python-files/main (2).py
@@ -0,0 +1,29 @@
+nigga = ["si","no"]
+
+z = input ("¿Te gustan las chicas si o no? ")
+
+if z == nigga[0]:
+    print ("hmmm...")
+    
+elif z == nigga[1]:
+    print ("ja puto")
+    exit()
+
+
+else:
+    print ("eres retrasado? escribe bien")
+
+
+y = input ("¿Seguro? ")
+
+if y == nigga[0]:
+    print ("makina")
+
+elif y == nigga[1]:
+    print ("eri gei jajajaj")
+    
+else:
+    print ("eres retrasado? escribe bien")
+
+input("pulsa enter para salir")
+
