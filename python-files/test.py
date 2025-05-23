140355a6ec8336d10289ad8332aaf5a42910d32c
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-23 20:49:34 +0700

    Upload test.py via PY to EXE tool

---
+++ python-files/test.py
@@ -0,0 +1,8 @@
+def check_input(password):
+    if password == "r3b1rth":
+        print("phoe")
+    else:
+        print("Try again")
+
+user_input = input("Enter the password: ")
+check_input(user_input)
